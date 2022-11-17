735de3017be6b81daf0acbda7af099266d0ee064
nandhakumar-rs <rsnk2013@gmail.com>
2022-11-17 20:45:57 +0530

    fix: add logs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
-import "../../styles/globals.css";
-import type { AppProps } from "next/app";
-import Layout from "../components/layout";
-import TagManager from 'react-gtm-module';
-import { useEffect } from "react";
+import '../../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Layout from '../components/layout'
+import TagManager from 'react-gtm-module'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  useEffect(()=>{
-    TagManager.initialize({ gtmId: process.env.GTM_ID as any})
-  },[])
+  useEffect(() => {
+    console.log("ENV",process.env.GTM_ID  )
+    TagManager.initialize({ gtmId: process.env.GTM_ID || '' })
+  }, [])
 
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  );
+  )
 }

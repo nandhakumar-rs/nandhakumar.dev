c4242f44d82c4df93486406317bad126edd4262d
nandhakumar-rs <rsnk2013@gmail.com>
2022-11-17 12:51:20 +0530

    feat: add hamburger menu

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,42 +1,54 @@
-import React, { FC } from "react";
-import GithubIcon from "./github.icon";
-import InstagramIcon from "./Instagram.icon";
-import LinkedInIcon from "./Linkedin.icon";
-import TwitterIcon from "./Twitter.icon";
+import React, { FC } from 'react'
+import CloseIcon from './close.icon'
+import GithubIcon from './github.icon'
+import InstagramIcon from './Instagram.icon'
+import LinkedInIcon from './Linkedin.icon'
+import MenuIcon from './menu.icon'
+import TwitterIcon from './Twitter.icon'
 
-export type TIconName = "instagram" | "github" | "twitter" | "linkedin";
+export type TIconName =
+  | 'instagram'
+  | 'github'
+  | 'twitter'
+  | 'linkedin'
+  | 'menu'
+  | 'close'
 
 interface ISVGProps {
-  color?: string;
+  color?: string
 }
 
 interface IIConProps {
-  name: TIconName;
-  svgProps?: ISVGProps;
+  name: TIconName
+  svgProps?: ISVGProps
 }
 
 const Icon: FC<IIConProps> = ({
-  svgProps = { color: "#000" },
-  name = "instagram",
+  svgProps = { color: '#000' },
+  name = 'instagram',
 }) => {
   const getIcon = () => {
     switch (name) {
-      case "instagram":
-        return renderIcon(InstagramIcon);
-      case "github":
-        return renderIcon(GithubIcon);
-      case "twitter":
-        return renderIcon(TwitterIcon);
-      case "linkedin":
-        return renderIcon(LinkedInIcon);
+      case 'instagram':
+        return renderIcon(InstagramIcon)
+      case 'github':
+        return renderIcon(GithubIcon)
+      case 'twitter':
+        return renderIcon(TwitterIcon)
+      case 'linkedin':
+        return renderIcon(LinkedInIcon)
+      case 'menu':
+        return renderIcon(MenuIcon)
+      case 'close':
+        return renderIcon(CloseIcon)
     }
-  };
+  }
 
   const renderIcon = (Component: any) => {
-    return <Component {...svgProps} />;
-  };
+    return <Component {...svgProps} />
+  }
 
-  return getIcon();
-};
+  return getIcon()
+}
 
-export default Icon;
+export default Icon
--- src/components/layout/Header.component.tsx
+++ src/components/layout/Header.component.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
-import Link from "next/link";
-import profile from "../../../public/profile.png";
-import CONSTANT from "../../constant";
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import profile from '../../../public/profile.png'
+import CONSTANT from '../../constant'
+import Icon from '../icons'
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false)
+  const router = useRouter()
   return (
-    <header className=" h-32 flex items-center justify-between">
+    <header className=" h-32 flex items-center justify-between relative">
       <Link href="/" className="flex items-center">
         <Image
           className="h-7"
@@ -16,7 +21,7 @@ const Header = () => {
         />
         <p className="font-bold text-2xl text-white ml-2">Nandhakumar</p>
       </Link>
-      <nav>
+      <nav className="max-md:hidden">
         {CONSTANT.ROUTES.map((route, index) => (
           <Link
             key={index}
@@ -27,8 +32,32 @@ const Header = () => {
           </Link>
         ))}
       </nav>
-    </header>
-  );
-};
 
-export default Header;
+      <div
+        className="hidden max-md:block absoute z-20 right-8 top-8"
+        onClick={() => setToggle(!toggle)}
+      >
+        <Icon name={!toggle ? 'menu' : 'close'} />
+      </div>
+
+      {toggle && (
+        <nav className="hidden max-md:flex flex-col bg-app-primary-900 h-full w-full fixed top-0 left-0 p-4 pt-20 z-10">
+          {CONSTANT.ROUTES.map((route, index) => (
+            <a
+              key={index}
+              className="font-bold text-base text-app-neutral-700 py-3 px-1 border-b border-app-primary-500"
+              onClick={() => {
+                setToggle(!toggle)
+                router.push(route.href)
+              }}
+            >
+              {route.label}
+            </a>
+          ))}
+        </nav>
+      )}
+    </header>
+  )
+}
+
+export default Header

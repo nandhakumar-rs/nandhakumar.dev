400d9d2f39cbf9c565767e8c8d0839ccd74825ce
nandhakumar-rs <rsnk2013@gmail.com>
2022-11-17 20:40:12 +0530

    feat: add google tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
+import TagManager from 'react-gtm-module';
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect(()=>{
+    TagManager.initialize({ gtmId: process.env.GTM_ID as any})
+  },[])
+
   return (
     <Layout>
       <Component {...pageProps} />

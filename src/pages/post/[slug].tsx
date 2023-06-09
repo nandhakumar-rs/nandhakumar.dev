982593a7492e2908334c81f1a9f42bdf340ad413
nandhakumar-rs <rsnk2013@gmail.com>
2023-06-09 19:45:39 +0530

    fix: meta tags

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -16,51 +16,53 @@ export default function PostPage({
     <section>
       <Head>
         <title>Post | {data?.title || ''}</title>
-        <meta key="title" name="title" content={data?.title || ''} />
-        <meta key="og:title" name="og:title" content={data?.title || ''} />
-        {/* <meta
-          key="twitter:title"
-          name="twitter:title"
-          content={data?.title || ''}
-        /> */}
-
-        <meta key="desc" name="description" content={data?.description || ''} />
+        <title>
+          Post | Using Handlebars Templates with SendGrid in Node.js
+        </title>
         <meta
-          key="og:desc"
-          name="og:description"
-          content={data?.description || ''}
+          name="title"
+          content="Post | Using Handlebars Templates with SendGrid in Node.js"
         />
-        {/* <meta
-          key="twitter:desc"
-          name="twitter:description"
-          content={data?.description || ''}
-        /> */}
+        <meta
+          name="description"
+          content="Learn how to how to use the Handlebars template engine to create dynamic email templates, and how to send these emails using SendGrid in a Node.js application"
+        />
 
-        <meta property="image" name="image" content={data?.thumbnailUrl} />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://www.nandhakumar.io/post/using-handlebar-with-sendgrid"
+        />
+        <meta
+          property="og:title"
+          content="Post | Using Handlebars Templates with SendGrid in Node.js"
+        />
+        <meta
+          property="og:description"
+          content="Learn how to how to use the Handlebars template engine to create dynamic email templates, and how to send these emails using SendGrid in a Node.js application"
+        />
         <meta
           property="og:image"
-          name="og:image"
-          content={data?.thumbnailUrl}
+          content="/images/post/using-handlebar-with-sendgrid.png"
         />
-        {/* <meta
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://www.nandhakumar.io/post/using-handlebar-with-sendgrid"
+        />
+        <meta
+          property="twitter:title"
+          content="Post | Using Handlebars Templates with SendGrid in Node.js"
+        />
+        <meta
+          property="twitter:description"
+          content="Learn how to how to use the Handlebars template engine to create dynamic email templates, and how to send these emails using SendGrid in a Node.js application"
+        />
+        <meta
           property="twitter:image"
-          name="twitter:image"
-          content={data?.thumbnailUrl}
+          content="/images/post/using-handlebar-with-sendgrid.png"
         />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@nandhakumar_io" />
-        <meta name="twitter:creator" content="@nandhakumar_io" /> */}
-                <meta name="twitter:card" content="summary_large_image" ></meta>
-        <meta name="twitter:site" content="@nandhakumar_io" ></meta>
-<meta name="twitter:title" content="Photo by Hyundai Motor Group on Unsplash" data-rh="true"></meta>
-
-<meta name="twitter:description" content="Genesis GV60 is charging at EV charging station – Download this photo by Hyundai Motor Group on Unsplash" data-rh="true"></meta>
-<meta name="twitter:url" content="https://www.nandhakumar.io/post/using-handlebar-with-sendgrid" data-rh="true"></meta>
-
-<meta name="twitter:image" content="https://images.unsplash.com/photo-1666919643134-d97687c1826c?crop=faces%2Cedges&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;ixid=M3wxMjA3fDF8MXxhbGx8MXx8fHx8fDJ8fDE2ODYzMTc0MDV8&amp;ixlib=rb-4.0.3&amp;q=60&amp;w=1200&amp;auto=format&amp;h=630&amp;mark-w=64&amp;mark-align=top%2Cleft&amp;mark-pad=50&amp;blend-mode=normal&amp;blend-alpha=10&amp;blend-w=1&amp;mark=https%3A%2F%2Fimages.unsplash.com%2Fopengraph%2Flogo.png&amp;blend=000000" data-rh="true"></meta>
-<meta name="twitter:creator" content="@hyundaimotorgroup" data-rh="true"></meta>
-        
       </Head>
       <Post
         data={data}

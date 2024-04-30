e99f158f03cd410030dcf449f2d75b0a24043132
nandhakumar-rs <rsnk2013@gmail.com>
2024-04-30 16:53:41 +0530

    feat: mastering webpack with typescript blog

--- src/components/mdx/code-snippet.component.tsx
+++ src/components/mdx/code-snippet.component.tsx
@@ -4,9 +4,15 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 const CodeSnippet = (props: any) => {
   return (
     <div className="relative my-8">
+      {props.filename && (
+        <div className="syntax-h font-bold text-sm px-4 py-1 rounded-t-lg font-mono w-max ml-auto mr-6">
+          {props.filename}
+        </div>
+      )}
+
       <SyntaxHighlighter
         showLineNumbers={props.showLineNumbers}
-        className="text-base rounded-md syntax-h"
+        className="text-base rounded-md syntax-h mt-0"
         language="javascript"
         style={oneDark}
         useInlineStyles={true}
